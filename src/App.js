597baf2339c918a50c0e2352837beeeae81549d6
Aysel Cavdar <a.misrikhanova13@gmail.com>
2021-10-25 01:34:55 +0300

    useState Basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
+import Setup from "./tutorial/1-useState/setup/1-error-example";
+import Final from "./tutorial/1-useState/final/2-useState-basics";
 function App() {
-  return <h2>advanced react tutorial</h2>;
+  return (
+    <div className="container">
+      <Setup />
+      <Final />
+    </div>
+  );
 }
 
 export default App;
